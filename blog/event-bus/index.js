c0e1279a6b2211d12dda232da73dacafbdc9f280
Giammarco <giammarco.boscaro@mailbox.org>
2023-06-29 21:28:16 +0200

    added event exchange between posts and comments

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -8,9 +8,12 @@ app.use(bodyParser.json());
 app.post("/events", (req, res) => {
     const event = req.body;
 
-    axios.post("http://localhost:4000/events", event);
-    axios.post("http://localhost:4001/events", event);
-    axios.post("http://localhost:4002/events", event);
+    axios.post("http://localhost:4000/events", event)
+        .catch(e => console.error(e.message));
+    axios.post("http://localhost:4001/events", event)
+        .catch(e => console.error(e.message));
+    axios.post("http://localhost:4002/events", event)
+        .catch(e => console.error(e.message));
 
     res.send({ status: "OK" });
 });
