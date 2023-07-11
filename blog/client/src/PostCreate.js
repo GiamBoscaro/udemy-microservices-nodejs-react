8d373b4d9687aa68b42df83da391d420164fb57b
Giammarco <giammarco.boscaro@mailbox.org>
2023-07-11 21:04:34 +0200

    configured paths with ingress url

--- blog/client/src/PostCreate.js
+++ blog/client/src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post("http://localhost:4000/posts", {
+    await axios.post("http://udemy.microservice-course.blog/posts", {
       title,
     });
 
