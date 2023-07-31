1e132081e9c43c8c02ae5fe246fb872b5d2b70e5
Giammarco <giammarco.boscaro@mailbox.org>
2023-07-31 20:46:30 +0200

    added mongo db

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -8,6 +8,7 @@ import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(json());
@@ -23,6 +24,17 @@ app.all('*', () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');    
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);    
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+}
+
+start();
