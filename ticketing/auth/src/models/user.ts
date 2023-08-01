ffef8fef6dd184fee3fb7777b8f78a08fd9b4394
Giammarco <giammarco.boscaro@mailbox.org>
2023-08-01 15:09:36 +0200

    added password hashing

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 interface IUser {
     email: string;
@@ -8,6 +9,18 @@ interface IUser {
 const schema = new mongoose.Schema<IUser>({
     email: { type: String, required: true },
     password: { type: String, required: true },
+});
+
+// Must NOT be an arrow function otherwise "this"
+// will be the outer context
+schema.pre('save', async function(done) {
+    // Mongoose considers a password modified
+    // also on the first insert
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    }
+    done();
 });
 
 const User = mongoose.model('User', schema);
