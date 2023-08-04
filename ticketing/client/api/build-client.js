efe9a1472936de9943cbc6dff432cf035afc31d1
Giammarco <giammarco.boscaro@mailbox.org>
2023-08-04 21:49:44 +0200

    implemented getInitialProps to fetch data before rendering

---
+++ ticketing/client/api/build-client.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export default ({ req }) => {
+  // Since we do not know if the call is being executed from the client or
+  // browser, we use this check to understand it
+  if (typeof window === 'undefined') {
+    // We are on the server
+    // The axios call will be executed from inside the container, in the k8s cluster
+    // so I need to point to the ingress
+    return axios.create({
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      // ingress doesn't proxy the host http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+      // but recognizes only udemy.microservice-course.ticketing
+      // But inside the req.headers there is the Host header that is: Host: udemy.microservice-course.ticketing
+      // so nginx will be able to proxy the request. Inside req there is also the cookie used for the auth
+      // For easyness, all client headers will be sent. Even if the call is executed on the server side,
+      // it will figure just as a call coming from the browser
+      headers: req.headers,
+    });
+  } else {
+    // We must be on the browser
+    // If we are on the browser, no particular thing to do. Everything will be handled by
+    // the browser, and the base URL will be the URL in the browser
+    return axios.create({
+      baseUrl: '/',
+    });
+  }
+};
--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,4 +1,28 @@
-const render = () => {
-  return <h1>Landing Page</h1>;
+import buildClient from '../api/build-client';
+
+// This is the React component. The instructions in here are
+// executed inside the browser. The baseurl of the axios call
+// executed here is the url in the browser (udemy.microservice-course.ticketing)
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
-export default render;
+
+// This function is used to fetch data before sending the page to
+// the browser. This is executed on the server side. The baseurl of the axios call
+// executed here is the localhost inside the Docker container (127.0.0.1:80)
+// BEWARE: in the scenario that we are navigating in the application without refreshing
+// (so using the Router to navigate) then getInitialProps WILL GET EXECUTED IN THE BROWSER!
+LandingPage.getInitialProps = async (context) => {
+  // context contains req, the request that is coming from the client requesting
+  // the html page, and so it contains many useful headers from the client
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default LandingPage;
