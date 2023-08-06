893998da25c5f73d442978db951e389b2853915b
Giammarco <giammarco.boscaro@mailbox.org>
2023-08-06 11:34:03 +0200

    updated commons library

--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { requireAuth, validateRequest } from '@rallycoding/common';
+import { requireAuth, validateRequest } from '@gboscaro-udemy-ticketing/common';
 import { ITicket, Ticket } from '../models/ticket';
 
 const router = express.Router();
