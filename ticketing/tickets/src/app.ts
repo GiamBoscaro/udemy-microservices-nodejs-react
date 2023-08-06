e156ebe6a48b98f22e4da0095529e0ceafef378e
Giammarco <giammarco.boscaro@mailbox.org>
2023-08-06 11:35:02 +0200

    added show ticket API

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -2,8 +2,9 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { currentUser, errorHandler, NotFoundError } from '@rallycoding/common';
+import { currentUser, errorHandler, NotFoundError } from '@gboscaro-udemy-ticketing/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -17,6 +18,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
