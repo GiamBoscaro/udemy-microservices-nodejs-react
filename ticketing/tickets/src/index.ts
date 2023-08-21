fa88525462a057fb2e03282a54b0c6888e82fc42
Giammarco <giammarco.boscaro@mailbox.org>
2023-08-21 20:30:23 +0200

    added nats client

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -11,15 +12,15 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect('ticketing', 'tickets-srv', 'http://nats-srv:4222');
     await mongoose.connect(process.env.MONGO_URI);
-
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log('Listening on port 3000');
+    console.log('Listening on port 3000!');
   });
 };
 
