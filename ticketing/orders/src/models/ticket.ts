8957275e04a904bdc3d08d9d8ce55ffc111d9e21
Giammarco <giammarco.boscaro@mailbox.org>
2023-08-25 16:08:36 +0200

    fixed ticket document interface

--- ticketing/orders/src/models/ticket.ts
+++ ticketing/orders/src/models/ticket.ts
@@ -4,20 +4,19 @@ import { Order, OrderStatus } from "./order";
 interface ITicket {
     title: string;
     price: number;
-    userId: string;
+}
+interface ITicketDocument extends ITicket {
     isReserved(): Promise<boolean>;  
 }
 
-const schema = new mongoose.Schema<ITicket>({
+const schema = new mongoose.Schema<ITicketDocument>({
     title: { type: String, required: true },
     price: { type: Number, required: true },
-    userId: { type: String, required: true },
 }, {
   toJSON: {
     transform(doc, ret) {
       ret.id = ret._id;
       delete ret._id;
-      delete ret.__v;
     },
   },
 });
