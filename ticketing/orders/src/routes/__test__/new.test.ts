a29a908ead07249cc8a533551a35d0ef7c67bf6a
Giammarco <giammarco.boscaro@mailbox.org>
2023-08-25 16:08:47 +0200

    added new order API unit tests

---
+++ ticketing/orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../../app';
+import { IOrder, Order, OrderStatus } from '../../models/order';
+import { ITicket, Ticket } from '../../models/ticket';
+
+it('returns an error if the ticket does not exist', async () => {
+  const ticketId = new mongoose.Types.ObjectId();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId })
+    .expect(404);
+});
+
+it('returns an error if the ticket is already reserved', async () => {
+  const ticket = new Ticket<ITicket>({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+  const order = new Order<IOrder>({
+    ticket,
+    userId: 'laskdflkajsdf',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
+
+it('reserves a ticket', async () => {
+  const ticket = new Ticket({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+});
