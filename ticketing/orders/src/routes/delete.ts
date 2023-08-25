17d137d966574b2914d9d9e8861c44680cb11a7e
Giammarco <giammarco.boscaro@mailbox.org>
2023-08-25 16:20:14 +0200

    added delete order API and tests

--- ticketing/orders/src/routes/delete.ts
+++ ticketing/orders/src/routes/delete.ts
@@ -1,9 +1,30 @@
 import express, { Request, Response } from 'express';
+import { Order, OrderStatus } from '../models/order';
+import { requireAuth, NotFoundError, NotAuthorizedError } from '@gboscaro-udemy-ticketing/common';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // publishing an event saying this was cancelled!
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
