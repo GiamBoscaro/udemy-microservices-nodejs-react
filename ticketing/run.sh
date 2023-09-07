a537adc3afe0fe8547ac091a41de5cd9d5999089
Giammarco <giammarco.boscaro@mailbox.org>
2023-09-07 19:03:11 +0200

    configured k8s for expiration service

--- ticketing/run.sh
+++ ticketing/run.sh
@@ -3,5 +3,7 @@
 # /etc/hosts
 #   127.0.0.1       udemy.microservice-course.ticketing
 
+# Fix for Cannot connect to the Docker daemon at unix:///var/run/docker.sock
+# sudo ln -s "$HOME/.docker/run/docker.sock" /var/run/docker.sock
 skaffold dev
 # navigate to http://udemy.microservice-course.ticketing
