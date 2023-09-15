6033bc62ba572e8f1ef8a3ff9a04af7a5d92f321
Giammarco <giammarco.boscaro@mailbox.org>
2023-09-15 17:06:41 +0200

    fixed expiration setting order as cancelled even if it was complete

--- ticketing/orders/src/events/listeners/expiration-complete-listener.ts
+++ ticketing/orders/src/events/listeners/expiration-complete-listener.ts
@@ -20,10 +20,15 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
       throw new Error('Order not found');
     }
 
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
+    }
+
     order.set({
       status: OrderStatus.Cancelled,
     });
     await order.save();
+
     await new OrderCancelledPublisher(this.client).publish({
       id: order.id,
       version: order.version,
